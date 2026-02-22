9f941001ec3678b750759088001e7cf5f486948f
fightformanhood <b5469001ster@gmail.com>
2026-02-21 21:02:37 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
-export default function () {
+export default function (eleventyConfig) {
+
+  // Copy admin folder so /admin works
+  eleventyConfig.addPassthroughCopy("src/admin");
+
+  // Copy uploads folder for CMS media
+  eleventyConfig.addPassthroughCopy("src/assets/uploads");
+
   return {
     dir: {
       input: "src",
       includes: "_includes",
       layouts: "_includes",
-      output: "dist",
-    },
-    markdownTemplateEngine: "njk"
+      output: "dist"
+    }
   };
 }
