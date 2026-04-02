aa38d6dfdc491ac71c337f84541322f12aba1ef0
fightformanhood <b5469001ster@gmail.com>
2026-04-02 14:34:42 -0400

    Update .eleventy.js.js

--- .eleventy.js.js
+++ .eleventy.js.js
@@ -1,6 +1,7 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/assets/uploads");
+  eleventyConfig.addPassthroughCopy("src/files");
 
   return {
     dir: {
